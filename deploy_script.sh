f8da8087e10d3865e198a3d5f1748029430594b4
Shusui MOYATANI <syusui.s@gmail.com>
2024-12-08 10:24:40 +0900

    deploy_script

---
+++ deploy_script.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -eux
+set -o pipefail
+
+export PATH="$HOME/local/golang/bin:/home/isucon/golang/bin:$PATH"
+
+BRANCH="${1:-"master"}"
+
+cd "$(dirname "$0")"
+if [ -d ".git" ]; then
+	git fetch
+	git checkout "$BRANCH"
+	git reset --hard origin/"$BRANCH"
+fi
+
+sudo test -f "/var/log/nginx/access.log" && sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.`date "+%Y%m%d_%H%M%S"`
+sudo test -f "/var/log/nginx/access_log.ltsv" && sudo mv /var/log/nginx/access_log.ltsv /var/log/nginx/access_log.`date "+%Y%m%d_%H%M%S"`.ltsv
+sudo test -f "/var/log/nginx/error.log" && sudo mv /var/log/nginx/error.log /var/log/nginx/error.log.`date "+%Y%m%d_%H%M%S"`
+sudo test -f "/var/log/mysql/mysql-slow.log" && sudo mv /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.`date "+%Y%m%d_%H%M%S"`
+
+cd go/
+make
+
+hostname="$(hostname)"
+case "${hostname}" in
+	"ip-192-168-0-11") # isucon1
+		sudo systemctl restart nginx
+		sudo systemctl restart isupipe-go.service
+		sudo systemctl restart mysql
+		;;
+	"ip-192-168-0-13") # isucon2
+		#sudo systemctl restart mysql
+		sudo systemctl restart nginx
+		sudo systemctl restart isupipe-go.service
+		sudo systemctl restart mysql
+		;;
+	"ip-192-168-0-12") # isucon3
+		sudo systemctl restart nginx
+		sudo systemctl restart isupipe-go.service
+		sudo systemctl restart mysql	
+		#sudo systemctl restart isupipe-go.service
+		;;
+	*)
+		echo "${hostname} Didn't match anything"
+		exit 1
+esac
